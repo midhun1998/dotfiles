b64f8141ae9158cb7abbaadfdd66aee8193aa783
midhun1998 <24776450+midhun1998@users.noreply.github.com>
2024-09-25 23:16:23 +0530

    Cleaned up unwanted files and added hidden files to ll

--- zsh/general-aliases.zsh
+++ zsh/general-aliases.zsh
@@ -1,5 +1,6 @@
 # General
 alias vimx='vim'
+alias ll='ls -latr'
 
 # Tmux
 alias t='tmux'
@@ -8,14 +9,6 @@ alias ta='tmux a -t'
 alias tls='tmux ls'
 alias tk='tmux kill-session -t'
 alias tr='tmux source-file ~/.tmux.conf'
-
-# Others
-alias opc='rlogin -l opcbuild swe-vm-kcr-proteus0$(($RANDOM % 7 + 1))'
-alias uxp='rlogin -l svc-uxmgr swe-vm-kcr-uxp0$(($RANDOM % 2 + 1))'
-alias scemgr='ssh -l svc-scemgr'
-alias scerun='ssh -l svc-sce-runner'
-alias sshme='ssh -l midhunn'
-alias python='/depot/Python-3.6.2/bin/python'
 
 # Kubernetes
 alias kgp="kubectl get pods "
